59afd0ed3215ac15056ff8213e11535c56cae041
blgissouf <bologo.issouf7@gmail.com>
2026-01-26 16:45:38 +0000

    Premier commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=", initial-scale=1.0">
+    <title>carte d'etudiant</title>
+</head>
+<body>
+    <h1>Bienvenu pour la generation de votre carte etudiant</h1>
+</body>
+</html>
